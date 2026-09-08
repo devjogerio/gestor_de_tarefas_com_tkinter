c168c5587cbb587f6307dd5eea4a712b3869fc90
Rogério Assunção <156693178+devjogerio@users.noreply.github.com>
2026-09-07 22:37:28 -0300

    update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from view.tarefa_view import TarefaView
 
 """Ponto de entrada da aplicação. Inicializa a interface gráfica principal."""
 
+
+
 if __name__ == "__main__":
     root = Tk()
     app = TarefaView(root)
